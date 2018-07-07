e0806301596be68d4c0fe33bc39c70a30c6e0820
Anthony Lee <anthony.s.lee96@gmail.com>
2018-07-07 19:06:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = 'hello!'
+  var lowercase = string
   if (string === lowercase.toLowerCase()) {
     return "I can't hear you!"
   }
