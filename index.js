6524b52b0e1393719e1d0d4d586f5f48abc2da7d
Anthony Lee <anthony.s.lee96@gmail.com>
2018-07-07 19:09:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,3 +27,9 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   }
 }
+
+function sayHiToGrandma(string) {
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.'
+  }
+}
