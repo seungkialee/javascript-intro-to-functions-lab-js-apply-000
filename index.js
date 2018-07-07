dc677009eb3c4116dcabf5514d2232056ba1fdb1
Anthony Lee <anthony.s.lee96@gmail.com>
2018-07-07 19:07:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,10 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
 }
+
+function sayHiToGrandma(string) {
+  var uppercase = string
+  if (uppercase.toUpperCase() === uppercase) {
+    return "YES INDEED!"
+  }
+}
